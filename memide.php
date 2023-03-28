9d9d96ba85e50a3c2088dcee39a2318b6bd4d061
Jose Novoa Rivera <36416017+Kharoontes@users.noreply.github.com>
2023-03-28 18:46:48 -0500

    Create memide.php

---
+++ memide.php
@@ -0,0 +1,12 @@
+<?php
+   $respuestas = array(
+       "vaya titan"
+       ,"mas largo que la torre eiffel"
+       ,"Te pasaste con ese buque de guerra"
+       ,"F"
+       ,"Lamentable"
+       ,"LLamas a ese titan jajaja"
+       );
+   $random = array_rand($respuestas);
+   echo $respuestas[$random];
+?>
